c89e8dd4c229df2af1381a9d6fcd7651302621fa
nscuro <nscuro@protonmail.com>
2023-11-05 21:00:59 +0100

    Add reproducer

--- src/main/java/mydomain/model/Property.java
+++ src/main/java/mydomain/model/Property.java
@@ -1,15 +1,12 @@
 package mydomain.model;
 
-import javax.jdo.annotations.Element;
 import javax.jdo.annotations.IdGeneratorStrategy;
-import javax.jdo.annotations.Join;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
-import java.util.List;
 
 @PersistenceCapable
-public class Person {
+public class Property {
 
     @PrimaryKey
     @Persistent(valueStrategy = IdGeneratorStrategy.NATIVE)
@@ -18,10 +15,8 @@ public class Person {
     @Persistent
     private String name;
 
-    @Persistent(defaultFetchGroup = "true", table = "persons_properties")
-    @Join(column = "person_id")
-    @Element(column = "property_id")
-    private List<Property> properties;
+    @Persistent
+    private String value;
 
     public long id() {
         return id;
@@ -39,11 +34,11 @@ public class Person {
         this.name = name;
     }
 
-    public List<Property> properties() {
-        return properties;
+    public String value() {
+        return value;
     }
 
-    public void setProperties(final List<Property> properties) {
-        this.properties = properties;
+    public void setValue(final String value) {
+        this.value = value;
     }
 }
--- src/test/java/org/datanucleus/test/SimpleTest.java
+++ src/test/java/org/datanucleus/test/SimpleTest.java
@@ -1,12 +1,16 @@
 package org.datanucleus.test;
 
-import java.util.*;
-import org.junit.*;
-import javax.jdo.*;
-
-import static org.junit.Assert.*;
-import mydomain.model.*;
+import mydomain.model.Person;
 import org.datanucleus.util.NucleusLogger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SimpleTest
 {
@@ -18,28 +22,47 @@ public class SimpleTest
 
         PersistenceManager pm = pmf.getPersistenceManager();
         Transaction tx = pm.currentTransaction();
-        try
-        {
+        try {
             tx.begin();
 
-            // [INSERT code here to persist object required for testing]
+            final Person person = new Person();
+            person.setName("John Doe");
+            pm.makePersistent(person);
 
             tx.commit();
-        }
-        catch (Throwable thr)
-        {
-            NucleusLogger.GENERAL.error(">> Exception in test", thr);
-            fail("Failed test : " + thr.getMessage());
-        }
-        finally 
-        {
-            if (tx.isActive())
-            {
+        } finally {
+            if (tx.isActive()) {
                 tx.rollback();
             }
-            pm.close();
         }
 
+        try {
+            tx.begin();
+
+            // Fetch list of persons to force bulk-fetch of properties collection.
+            // Because no properties were assigned, DataNucleus will assign an empty
+            // ArrayList instance to the properties field.
+            // https://github.com/datanucleus/datanucleus-rdbms/commit/7fc3a65c96eb8913c3e2fe5745c668c57328e0e1
+            final List<Person> persons = pm.newQuery(Person.class).executeList();
+
+            Assert.assertEquals(1, persons.size());
+
+            // Assigning a list implementation other than ArrayList will cause a ClassCastException, e.g.:
+            //   java.lang.ClassCastException: class java.util.LinkedList cannot be cast to class java.util.ArrayList
+
+            // This works:
+            //   persons.get(0).setProperties(new ArrayList<>());
+            // But this doesn't:
+            persons.get(0).setProperties(new LinkedList<>());
+
+            tx.commit();
+        } finally {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+        }
+
+        pm.close();
         pmf.close();
         NucleusLogger.GENERAL.info(">> test END");
     }
